812f286ec5a48217018701e014743beb2b3ed8af
jaskaran26 <jasskaranofficial@gmail.com>
2025-09-01 12:00:00 +0530

    fixed api config

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,5 +1,7 @@
 const APP_ENVIRONMENT = process.env.NEXT_PUBLIC_ENV;
 
+export const isDev = APP_ENVIRONMENT === 'staging' || APP_ENVIRONMENT === 'production' ? false : true;
+
 const API_BASE_URL = APP_ENVIRONMENT === 'staging' ? 'https://staging.osmrtnica.com/api' :
   APP_ENVIRONMENT === 'production' ? 'https://dev111.osmrtnica.com/be/api' : 'http://localhost:4000/api';
 

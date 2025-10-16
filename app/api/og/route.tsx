f46197b7446a67472cb62417deecedd65ba91e06
uzair888 <uzairsarwar10@gmail.com>
2025-10-17 01:17:34 +0500

    fixed: Image size

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -6,29 +6,31 @@ import sharp from "sharp";
 
 export const runtime = "nodejs";
 
-function formatObituaryDate(dateString?: string) {
-  if (!dateString) return "";
-  const date = new Date(dateString);
-  return date.getFullYear().toString();
-}
+async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}:opsz,wght@8..144,100..1000&display=swap&text=${encodeURIComponent(
+    text
+  )}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/
+  );
 
-function calculateAge(birthDate?: string, deathDate?: string) {
-  if (!birthDate || !deathDate) return "";
-  const birth = new Date(birthDate);
-  const death = new Date(deathDate);
-  let age = death.getFullYear() - birth.getFullYear();
-  if (
-    death.getMonth() < birth.getMonth() ||
-    (death.getMonth() === birth.getMonth() && death.getDate() < birth.getDate())
-  ) {
-    age--;
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
   }
-  return age;
+
+  throw new Error("failed to load font data");
 }
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const slugKey = searchParams.get("slugKey");
+  const fontData = await fetch(
+    new URL("/fonts/GreatVibes-Regular.ttf", req.url)
+  ).then((res) => res.arrayBuffer());
 
   const obituary = await fetch(
     `${API_BASE_URL}/obituary/memory?slugKey=${slugKey}`
@@ -36,11 +38,6 @@ export async function GET(req: Request) {
     const data = await res.json();
     return data.obituary;
   });
-
-  const formattedBirthDate = formatObituaryDate(obituary?.birthDate);
-  const formattedDeathDate = formatObituaryDate(obituary?.deathDate);
-
-  const age = calculateAge(obituary?.birthDate, obituary?.deathDate);
 
   const imageUrl = obituary?.image || `${APP_BASE_URL}/user5.jpeg`;
 
@@ -58,25 +55,25 @@ export async function GET(req: Request) {
     return new Response("Not found", { status: 404 });
   }
 
+  const name = `${obituary?.name} ${obituary?.sirName}`;
+  const location = `Trebelno`;
+
   return new ImageResponse(
     (
       <div
         style={{
           position: "relative",
           width: "1200px",
-          height: "auto",
+          height: "630px",
           display: "flex", // ✅ Required for multiple children
           flexDirection: "column", // stack card + footer vertically
           justifyContent: "center",
           alignItems: "center",
-          objectFit: "cover",
-          backgroundRepeat: "no-repeat",
         }}
       >
         <img
-          src={`${APP_BASE_URL}/fb-posting-hero-bg.png`}
+          src={`${APP_BASE_URL}/fb-post-bg.png`}
           alt="Memory page hero background"
-          className="mobile:hidden h-auto mobile:h-[315px] w-full object-cover"
           style={{
             width: "100%",
             height: "100%",
@@ -87,7 +84,7 @@ export async function GET(req: Request) {
           style={{
             display: "flex",
             alignItems: "center",
-            gap: "40px",
+            gap: "45px",
             padding: "30px 50px",
             borderRadius: "16px",
             width: "100%",
@@ -98,19 +95,19 @@ export async function GET(req: Request) {
           {/* Left image */}
           <div
             style={{
-              width: "180px",
-              height: "250px",
+              width: "175px",
+              height: "216px",
               borderRadius: "12px",
               overflow: "hidden",
-              boxShadow: "0 4px 20px rgba(0,0,0,0.15)",
               display: "flex", // ✅ required if img inside
+              border: "4px solid white",
             }}
           >
             <img
               src={convertedImageUrl}
               alt="Obituary"
-              width={180}
-              height={250}
+              width={175}
+              height={216}
               style={{
                 objectFit: "cover",
                 borderRadius: "12px",
@@ -123,120 +120,77 @@ export async function GET(req: Request) {
             style={{
               display: "flex",
               flexDirection: "column",
-              justifyContent: "center",
-              flex: 1,
+              justifyContent: "space-between",
+              height: "98%",
             }}
           >
             <div
               style={{
                 display: "flex",
-                fontSize: "36px",
-                fontWeight: 400,
-                color: "#414141",
-                marginBottom: "10px",
-                fontFamily: "Great Vibes, cursive",
+                fontSize: "48px",
+                color: "#414B5A",
+                fontFamily: "GreatVibes",
               }}
             >
               Za vedno v naših srcih
             </div>
-
             <div
               style={{
-                fontSize: "54px",
-                fontWeight: 600,
-                color: "#1E2125",
-                marginBottom: "10px",
+                fontSize: "64px",
+                fontWeight: 500,
+                color: "#292424",
                 display: "flex",
+                fontFamily: "Roboto Flex",
+                letterSpacing: "-0.01em",
               }}
             >
-              {obituary?.name} {obituary?.sirName}
+              {name}
             </div>
-
             <div
               style={{
-                fontSize: "28px",
-                color: "#414141",
-                marginBottom: "8px",
                 display: "flex",
+                flexDirection: "column",
               }}
             >
-              {formattedBirthDate && formattedDeathDate
-                ? `${formattedBirthDate} – ${formattedDeathDate} (${age} let)`
-                : formattedDeathDate}
-            </div>
-
-            <div
-              style={{
-                fontSize: "28px",
-                color: "#414141",
-                display: "flex",
-              }}
-            >
-              {obituary?.location}
+              <div
+                style={{
+                  fontSize: "40px",
+                  color: "#3C3E41",
+                  display: "flex",
+                  fontFamily: "Roboto Flex",
+                  letterSpacing: "-0.01em",
+                }}
+              >
+                {location}
+              </div>
             </div>
           </div>
         </div>
-        <div
-          style={{
-            position: "absolute",
-            display: "flex",
-            cursor: "pointer",
-            justifyContent: "center",
-            gap: "16px", // equivalent to mobile gap-3 (0.75rem)
-            borderRadius: "6px",
-            border: "1px solid #ffffff",
-            alignItems: "center",
-            width: "auto",
-            height: "48px",
-            background:
-              "linear-gradient(to right, #FFFFFF, rgba(255, 255, 255, 0.3))",
-            bottom: "12%",
-            right: "3%",
-            paddingLeft: "22px",
-            paddingRight: "22px",
-          }}
-        >
-          <p
-            style={{
-              fontFamily: "Roboto Flex",
-              color: "#3C3E41",
-              fontSize: "22px",
-            }}
-          >
-            Vpis v žalno knjigo in informacije o pogrebu so tukaj
-          </p>
-          <img
-            src={`${APP_BASE_URL}/previous_img.png`}
-            style={{
-              transform: "rotate(180deg)",
-              height: "22.34px",
-              width: "15.23px",
-              marginTop: "4px",
-            }}
-          />
-        </div>
-
-        {/* Footer */}
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-start",
-            width: "100%",
-            position: "absolute",
-            bottom: "10px",
-            left: "20px",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "20px",
-              color: "rgba(0,0,0,0.5)",
-            }}
-          >
-            osmrtnica.com
-          </p>
-        </div>
       </div>
-    )
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "GreatVibes",
+          data: fontData,
+          style: "normal",
+          weight: 400,
+        },
+        {
+          name: "Roboto Flex",
+          data: await loadGoogleFont("Roboto Flex", name),
+          style: "normal",
+          weight: 500,
+        },
+        {
+          name: "Roboto Flex",
+          data: await loadGoogleFont("Roboto Flex", location),
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
   );
 }

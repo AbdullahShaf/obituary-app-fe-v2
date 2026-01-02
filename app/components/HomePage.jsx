094818afb358604a31e72db886a0b3255ec15af4
macbook <macbook@192.168.1.17>
2026-01-02 14:40:55 +0500

    Add new homepage updates

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -495,7 +495,7 @@ export default function HomeContent(props) {
               </div>
               <div className="relative">
                 <SelectDropdown
-                  label={"Občina"}
+                  label={"Občin"}
                   isFromFlower={true}
                   isFromNotification={false}
                   isFrom={"mainPage"}
